783a65ac7f01709fc4b18b562fd7f622f3fcdd59
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2023-11-14 14:53:31 +0700

    feat(script): 14112023

--- src/scripts/views/movie.ts
+++ src/scripts/views/movie.ts
@@ -1,6 +1,3 @@
-import { Movie } from '@/interfaces';
-import { USER_ID } from '@/constants';
-
 export class MovieView {
   constructor() {}
 
@@ -8,23 +5,20 @@ export class MovieView {
    * Binds a add event handler to the add favourites button.
    * @param {Function} handler - The add event handler function.
    */
-  bindAddFavouritesEvent = (
-    handler: (id: string, isCreated: boolean, element: HTMLElement) => void,
-  ) => {
-    const buttonAddMovieIntoFavourites = document.querySelectorAll<HTMLElement>(
-      '.add-movie-into-favourites',
-    );
+  bindAddOrRemoveFavouritesEvent = (handler: (id: string, element: HTMLElement) => void) => {
+    const buttonAddOrRemoveFavouritesMovie =
+      document.querySelectorAll<HTMLElement>('.button-heart-card');
 
-    buttonAddMovieIntoFavourites.forEach((element: HTMLElement) => {
+    buttonAddOrRemoveFavouritesMovie.forEach((element: HTMLElement) => {
       element.addEventListener('click', (event: MouseEvent) => {
         const targetElement = event.target as HTMLElement;
 
         if (targetElement) {
-          const parentFigure: HTMLElement | null = targetElement.closest('figure');
-          const movieId = parentFigure?.getAttribute('id');
+          const parentFigure = targetElement.closest('figure') as HTMLElement;
+          const movieId = parentFigure.getAttribute('id');
 
           if (movieId) {
-            handler(movieId, true, targetElement);
+            handler(movieId, targetElement);
           }
         }
       });
